ea4b200cac96385598904210015b7449fb523395
J <0xjariel@protonmail.com>
2023-09-25 01:10:04 -0600

    init

---
+++ next-app/app/components/ProductCard.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ProductCard = () => {
+  return (
+    <div>
+      <button
+        onClick={() => {
+          console.log("Click");
+        }}
+      ></button>
+    </div>
+  );
+};
+
+export default ProductCard;
