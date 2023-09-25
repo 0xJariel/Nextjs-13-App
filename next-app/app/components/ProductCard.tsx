513d606929232b220adabfdc1e2504e6f8d7f74a
J <0xjariel@protonmail.com>
2023-09-25 01:16:24 -0600

    client v server components

--- next-app/app/components/ProductCard.tsx
+++ next-app/app/components/ProductCard.tsx
@@ -1,13 +1,11 @@
+"use client";
 import React from "react";
+import AddToCart from "./AddToCart";
 
 const ProductCard = () => {
   return (
     <div>
-      <button
-        onClick={() => {
-          console.log("Click");
-        }}
-      ></button>
+      <AddToCart />
     </div>
   );
 };
